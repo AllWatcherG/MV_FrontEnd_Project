d40f1317b0b9823bec5896d8806165a7d4ddb8b0
AllWatcherG <92951041+AllWatcherG@users.noreply.github.com>
2023-11-06 12:47:58 -0500

    working get all HARRYPOTTER Characters

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const express  = require("express")
-const app = express()
+const express = require('express');
+const app = express();
+const port = 3000; // You can choose a different port
 
-app.get('/', (req, res) => {
-    console.group("hi")
-    res.sendStatus(500)
-    res.send("hi")
-})
+// Define a GET route to fetch data from the Harry Potter API
+app.get('/get_hp_characters', async (req, res) => {
+  const api_url = 'https://hp-api.onrender.com/api/characters';
+  
+  try {
+    const response = await fetch(api_url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data)
+      res.json(data);
+    } else {
+      res.status(500).json({ error: 'Failed to fetch data from the API' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-app.listen(3001)
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
